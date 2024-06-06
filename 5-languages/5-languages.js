a700f92b98c7021e5ef70f6407b864327453fcd3
Arina <arinaver_1991@mail.ru>
2024-06-07 01:46:05 +0300

    5-languages.js

---
+++ 5-languages/5-languages.js
@@ -0,0 +1,45 @@
+const langUserRU = 'Русский';
+const langUserEN = 'Английский';
+const langUserSPA = 'Испанский';
+
+switch (langUserRU) {
+    case 'Русский':
+        console.log('Привет');
+        break;
+    case 'Английский':
+        console.log('Hello');
+        break;
+    case 'Испанский':
+        console.log('Hola');
+        break;  
+    default:
+        console.log('Не используется язык');    
+}
+
+switch (langUserEN) {
+    case 'Русский':
+        console.log('Привет');
+        break;
+    case 'Английский':
+        console.log('Hello');
+        break;
+    case 'Испанский':
+        console.log('Hola');
+        break;  
+    default:
+        console.log('Не используется язык');    
+}
+
+switch (langUserSPA) {
+    case 'Русский':
+        console.log('Привет');
+        break;
+    case 'Английский':
+        console.log('Hello');
+        break;
+    case 'Испанский':
+        console.log('Hola');
+        break;  
+    default:
+        console.log('Не используется язык');    
+}
