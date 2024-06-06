f03e9b89ab563afc4be661a33be666eb95da9ad7
Arina <arinaver_1991@mail.ru>
2024-06-07 01:37:05 +0300

    4-navigator.js

---
+++ 4-navigator.js/4-navigator.js
@@ -0,0 +1,5 @@
+const adressLat = 38;
+const adressLong = 26;
+const positionLat = 64;
+const positionLong = 57;
+console.log(Math.sqrt((adressLat - positionLat)**2 + (adressLong - positionLong)**2));
