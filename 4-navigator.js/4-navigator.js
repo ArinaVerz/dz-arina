aa2c7ae4a5ecdc1047e7735cb9213550f55c98ec
Arina <arinaver_1991@mail.ru>
2024-06-07 15:56:57 +0300

    4-navigator.js исправление

--- 4-navigator.js/4-navigator.js
+++ 4-navigator.js/4-navigator.js
@@ -2,4 +2,6 @@ const adressLat = 38;
 const adressLong = 26;
 const positionLat = 64;
 const positionLong = 57;
-console.log(Math.sqrt((adressLat - positionLat)**2 + (adressLong - positionLong)**2));
+
+const res = Math.sqrt((adressLat - positionLat)**2 + (adressLong - positionLong)**2);
+console.log(res);
