0c7351cc7be815f91c05152cf75feed222a8d7a5
Arina <arinaver_1991@mail.ru>
2024-12-19 00:59:57 +0300

    12-luna

--- 12-luna/12-luna.js
+++ 12-luna/12-luna.js
@@ -1,27 +1,27 @@
 function luhnCheck(str) {
-  const nCheck = str.replace(/\D/g, "");
-  if (nCheck.length != 16) {
-    return false;
-  }
-  const arrayWithEvenNumberX2 = nCheck
-    .split("")
-    .reverse()
-    .map((item, i) => {
-      if (i % 2 !== 0) {
+    const nCheck = str.replace(/\D/g, "");
+    if (nCheck.length != 16) {
+        return false;
+    }
+    const arrayWithEvenNumberX2 = nCheck
+        .split("")
+        .reverse()
+        .map((item, i) => {
+    if (i % 2 !== 0) {
         const numX2 = item * 2;
-        if (numX2 > 9) {
-          return numX2 - 9;
+            if (numX2 > 9) {
+            return numX2 - 9;
         }
         return numX2;
-      }
-      return item;
+        }
+        return item;
     });
   const sum = arrayWithEvenNumberX2.reduce((acc, item) => {
-    return (acc += Number(item));
+        return (acc += Number(item));
   }, 0);
   if (sum) {
-    return sum % 10 === 0 ? true : false;
-  }
+        return sum % 10 === 0 ? true : false;
+    }
 }
 
 console.log(luhnCheck("4539-1488-0343-6467"));
